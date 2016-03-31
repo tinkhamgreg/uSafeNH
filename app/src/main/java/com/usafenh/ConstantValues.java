879db993f9ef31f911e918dde2cffe19c02e5f70
tinkhamgreg <gtinkham@gmail.com>
2016-03-31 08:07:27 -0400

    homescreen additions, other improvements

--- app/src/main/java/com/usafenh/ConstantValues.java
+++ app/src/main/java/com/usafenh/ConstantValues.java
@@ -7,6 +7,7 @@ public class ConstantValues {
     public static int SCHOOL_PLYMOUTH_STATE = R.string.Plymouth_State;
     public static int SCHOOL_SAINT_ANSELM = R.string.Saint_Anselm;
     public static int SCHOOL_KEENE_STATE = R.string.Keene_State;
+    public static int SCHOOL_WHITE_MOUNTAIN =R.string.White_Mountain;
 
     public static String USER_TOKEN = "userResourceID";
     public static int USER_SELF = R.string.help_now_button;
