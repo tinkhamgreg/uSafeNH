1bc640d709f0dc002f0bec6f26a45d594196110e
tinkhamgreg <gtinkham@gmail.com>
2016-04-10 17:19:41 -0400

    ui improvements

--- app/src/main/java/com/usafenh/HelpActivity.java
+++ app/src/main/java/com/usafenh/HelpActivity.java
@@ -3,10 +3,16 @@ package com.usafenh;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 
-public class HelpActivity extends Activity {
+
+public class HelpActivity extends MainActivity
+        implements NavigationView.OnNavigationItemSelectedListener{
 
     private static final String TAG = "HelpActivity";
 
@@ -27,6 +33,18 @@ public class HelpActivity extends Activity {
         setTitle(userData.getSchoolID());
 
         setButtonListeners();
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     private void setButtonListeners() {
