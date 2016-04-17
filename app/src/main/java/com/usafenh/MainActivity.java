d888f4b54a480c3ac4a1c260c45d24d40cd23777
tinkhamgreg <gtinkham@gmail.com>
2016-04-17 11:48:32 -0400

    added all questions, ui improvements.

--- app/src/main/java/com/usafenh/MainActivity.java
+++ app/src/main/java/com/usafenh/MainActivity.java
@@ -24,6 +24,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
 import java.util.UUID;
 
